f27cba3cf70933b36f72bca2c3786cc092ad0b76
mirucon <i@miruc.co>
2018-07-03 22:36:50 +0900

    initial commit

---
+++ handbook.js
@@ -0,0 +1,54 @@
+const axios = require('axios')
+const program = require('commander')
+const fs = require('fs')
+
+const getTotalPages = async (team, handbook) => {
+  handbook = handbook ? handbook : 'handbook'
+  const res = await axios.get(
+    `https://make.wordpress.org/${team}/wp-json/wp/v2/${handbook}?per_page=100`
+  )
+  return res.headers['x-wp-totalpages']
+}
+
+const generateJson = async (team, handbook) => {
+  handbook = handbook ? handbook : 'handbook'
+  const totalPages = await getTotalPages(team, handbook)
+  const data = []
+  for (let i = 0; i < totalPages; i++) {
+    const page = i + 1
+    await axios
+      .get(
+        `https://make.wordpress.org/${team}/wp-json/wp/v2/${handbook}?per_page=100&page=${page}`
+      )
+      .then(res => {
+        console.log(res)
+        for (const item of res.data) {
+          data.push({
+            slug: item.slug,
+            link: item.link,
+            modified: item.modified_gmt,
+            menu_order: item.menu_order,
+            parent: item.parent
+          })
+        }
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
+
+  fs.writeFile(`handbook.json`, JSON.stringify(data), err => {
+    if (err) throw err
+  })
+}
+
+program
+  .version('1.0.0')
+  .command('handbook <team>')
+  .description('Generate a menu JSON file for WordPress.org handbook')
+  .option('--handbook <handbook>', 'Specify handbook name')
+  .action((team, options) => {
+    generateJson(team, options.handbook)
+  })
+
+program.parse(process.argv)
