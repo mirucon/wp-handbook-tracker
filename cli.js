f1e898409cfddd9602f1c17ecb032922b793d53a
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-07-27 06:48:11 +0900

    use node-wpapi

--- cli.js
+++ cli.js
@@ -2,53 +2,54 @@
 
 'use strict'
 
-const axios = require('axios')
 const program = require('commander')
 const mkdirp = require('mkdirp')
 const fs = require('fs')
+const _ = require( 'lodash' )
+const WPAPI = require( 'wpapi' )
 
-mkdirp('api/v1', err => {
-  if (err) console.log(err)
-})
+const wp = new WPAPI( { endpoint: 'https://make.wordpress.org/cli/wp-json' } );
 
-const getTotalPages = async (team, handbook) => {
-  handbook = handbook ? handbook : 'handbook'
-  const res = await axios.get(
-    `https://make.wordpress.org/${team}/wp-json/wp/v2/${handbook}?per_page=100`
-  )
-  return res.headers['x-wp-totalpages']
+mkdirp( 'api/v1', err => {
+  if ( err ) console.log( err )
+} )
+
+
+const getAll = ( request ) => {
+  return request.then( ( response ) => {
+    if ( ! response._paging || ! response._paging.next ) {
+      return response;
+    }
+    // Request the next page and return both responses as one collection
+    return Promise.all( [
+      response,
+      getAll( response._paging.next )
+    ] ).then(function( responses ) {
+      return _.flatten( responses );
+    } );
+  } );
 }
 
 const generateJson = async (team, handbook) => {
   handbook = handbook ? handbook : 'handbook'
-  const totalPages = await getTotalPages(team, handbook)
-  const data = []
-  for (let i = 0; i < totalPages; i++) {
-    const page = i + 1
-    await axios
-      .get(
-        `https://make.wordpress.org/${team}/wp-json/wp/v2/${handbook}?per_page=100&page=${page}`
-      )
-      .then(res => {
-        console.log(res)
-        for (const item of res.data) {
-          data.push({
-            slug: item.slug,
-            link: item.link,
-            modified: item.modified_gmt,
-            menu_order: item.menu_order,
-            parent: item.parent
-          })
-        }
-      })
-      .catch(e => {
-        console.log(e)
-      })
-  }
+  wp.handobooks = wp.registerRoute( 'wp/v2', '/' + handbook + '/(?P<id>)' );
 
-  fs.writeFile(`api/v1/${team}-${handbook}.json`, JSON.stringify(data), err => {
-    if (err) throw err
-  })
+  getAll( wp.handobooks() ).then( ( allPosts ) => {
+    const data = [];
+    for ( const item of allPosts ) {
+      data.push( {
+        slug: item.slug,
+        link: item.link,
+        modified: item.modified_gmt,
+        menu_order: item.menu_order,
+        parent: item.parent
+      } )
+    }
+
+    fs.writeFile( `api/v1/${team}-${handbook}.json`, JSON.stringify( data ), err => {
+      if ( err ) throw err
+    } )
+  } );
 }
 
 program
